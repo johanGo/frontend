7efc26006ce2fca7cadf658060aff1767d1339f8
yofreOrmaza <ormaza.main@gmail.com>
2023-12-26 17:16:43 -0500

    chore: Commit a fronted

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,36 @@
+<template>
+   <Table />
+</template>
+
+
+<script>
+import Table from '../components/Table.vue'
+
+
+export default {
+    name:'Products',
+
+    components:{
+        Table,
+    },
+    created(){
+        this.getProductos()
+    },
+    methods:{
+        getProductos: async function(){
+            try {
+                const setting= {
+                    method: 'GET',
+                }
+                const url =  'http://localhost:3000/api';
+                const data =await fetch(url, setting);
+                const json =await data.json();
+                console.log(json)
+            } catch (error) {
+                
+            }
+        }
+    }
+
+}
+</script>
